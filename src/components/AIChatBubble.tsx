5713a85b05409eb54d9d86288bef6c2ba0c0fdf8
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-04-30 21:31:45 +0300

    add AIChatBubble component

---
+++ src/components/AIChatBubble.tsx
@@ -0,0 +1,14 @@
+
+
+const AIChatBubble = ({message}: {message: string}) => {
+    return (
+        <div className="flex 'justify-end'">
+            <div
+                className="max-w-md text-wrap text-slate-900 bg-casablanca-500 shadow-sm  rounded-2xl px-4 py-3">
+                <p className="leading-relaxed text-black">{message}</p>
+            </div>
+        </div>
+    )
+}
+
+export default AIChatBubble
