c936dc64585964374b651f53b35fa2786b3b5106
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-05-01 11:11:02 +0300

    feat: render markdown using dangerouslySetInnerHTML

--- src/components/AIChatBubble.tsx
+++ src/components/AIChatBubble.tsx
@@ -2,12 +2,27 @@
 
 const AIChatBubble = ({message, isError,  isLoading}: {message: string, isError: boolean, isLoading: boolean},  ) => {
     return (
-        <div className="flex 'justify-end'">
-            <div
-                className="max-w-md text-wrap text-slate-900 bg-casablanca-500 shadow-sm  rounded-2xl px-4 py-3">
-                <p className="leading-relaxed text-black">{message}</p>
-            </div>
-        </div>
+            !isError && (
+                <div className="flex 'justify-end'">
+                    <div
+
+                        className="max-w-md text-wrap text-slate-900 bg-casablanca-400 shadow-sm  rounded-2xl px-4 py-3">
+                        {
+                            isLoading?
+                                ( <span  className="loading loading-dots loading-xl"></span>)
+                                :
+                                (
+                                    <p dangerouslySetInnerHTML={{ __html: message }}
+                                       className="leading-relaxed text-black"
+                                    />
+                                )
+                        }
+
+
+                    </div>
+                </div>
+            )
+
     )
 }
 
