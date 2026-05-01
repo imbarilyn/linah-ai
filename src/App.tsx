400ce631ddd73477ac8288ee629210be56b3763b
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-05-01 12:44:22 +0300

    feat: add App

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ChatPage from "./chatbot/ChatPage.tsx";
+
+function App() {
+
+
+  return (
+    <div className=" bg-dark-one">
+        <ChatPage />
+
+    </div>
+  )
+}
+
+export default App
